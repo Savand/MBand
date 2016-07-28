c41ece7150de6a4113779079ddb6ce671f501590
Andrew <savka.and@gmail.com>
2016-07-28 09:18:17 +0300

    Delete MailSender.java

--- src/main/java/com/andsav/gk/mail/MailSender.java
+++
@@ -1,9 +0,0 @@
-package com.andsav.gk.mail;
-
-import javax.mail.MessagingException;
-
-public interface MailSender {
-
-	void send(String to, String subject, String body)  throws MessagingException;
-
-}
