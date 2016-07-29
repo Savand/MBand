6f5ddcf4fbcad18ef4d92aaa060d97ec55140000
Savand <savka.and@gmail.com>
2016-07-29 13:28:13 +0300

    Improved mailsender, added bootstrap framework for frontend developement etc.

--- src/main/java/com/andsav/gk/controllers/RootController.java
+++ src/main/java/com/andsav/gk/controllers/RootController.java
@@ -1,25 +1,10 @@
 package com.andsav.gk.controllers;
 
 
-import javax.mail.MessagingException;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.andsav.gk.mail.MailSenderSmtp;
-
-@RestController
+@Controller
 public class RootController {
 	
-	@Autowired
-	MailSenderSmtp sender;
-	
-	@RequestMapping(path="/sendMail")
-	public void home() throws MessagingException{
-		sender.send("savka.and@gmail.com", "hello", "how are you?");
-		
-	}
-
 	
 }
